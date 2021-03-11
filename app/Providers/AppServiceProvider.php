7b252fbde98b07bddcb8afa029c02b494c02669a
Thinh Nguyen <misddev@gmail.com>
2021-03-11 23:30:02 +0700

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
 
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
     }
 
     /**
@@ -23,7 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 
 }
